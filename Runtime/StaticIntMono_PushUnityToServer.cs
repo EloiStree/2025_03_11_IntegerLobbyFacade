1831692f1bb33d22af5fc44ccde488a1d0480795
Éloi Strée <eloistree@gmail.com>
2025-04-09 00:51:57 +0200

    Save: 2025_04_09_00_51

--- Runtime/StaticIntMono_PushUnityToServer.cs
+++ Runtime/StaticIntMono_PushUnityToServer.cs
@@ -6,9 +6,17 @@ namespace Eloi.IntegerLobby
 {
     public class StaticIntMono_PushUnityToServer : MonoBehaviour
     {
-        public void  PushIntegerUnityToServer(int value) { 
-        
+        public void PushIntegerUnityToServer(int value) {
+
             StaticIntegerLobby.PushIntegerUnityToServer(value);
+        }
+    
+        public void PushIntegerUnityToServer(string valueToPush) {
+
+            if (int.TryParse(valueToPush, out int value))
+            {
+                PushIntegerUnityToServer(value);
+            }
         }
     }
 
